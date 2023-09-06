269a659cc233c4e174fa27752fbbeef1e123987b
Andrzej Sidor <asidor23@gmail.com>
2023-09-06 23:02:04 +0200

    Commit #3

--- if.sh
+++ if.sh
@@ -4,18 +4,13 @@ echo "Podaj swoje imie: ";
 read imie;
 
 if [ $imie = "Andrzej" ]
-then
-  echo "Imie to Andrzej";
+then  
+    echo "Imie to Andrzej";
 elif[ $imie = "Anna" ]
 then
   echo "Imie to Anna";
 else
   echo "Nie znam Cie";
 fi
-
-
-
-
-
 
 echo $imie;
