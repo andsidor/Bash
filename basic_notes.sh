847d593dcbd99846256e5667fa11d8666e5885f5
Andrzej <asidor23@gmail>
2024-09-29 22:59:09 +0200

    Add file hello word.sh

---
+++ basic_notes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+#1.Command cd -  change directory
+
+
+
+
+
+echo "Wprowadz swoje imie: ";
+
+read imie;
+
+echo "witaj $imie";
+
+
+
+# Create directory in local folder
+
+mkdir  test_folder
+
+
+
---
+++ hello_world.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#Creates a new variable with a value of "Hello World"
+
+
+echo "1.___Hello_World___"
+
+learningbash="Hello World"
+echo $learningbash
+
+echo "___________________________________________"
+echo " "
+
+echo "2.___Echo_Command___"
+
+var="variable"
+echo 'Echo with 'var' is $var'
+echo "Echo with "var" is $var"
+echo "___________________________________________"
+echo " "
+#Sleep  number of seconds - build in fuction
+#Wait  process_ID(if default wait until all child process are competed - build in fuction 
+echo "___________________________________________"
+echo " "
+echo "3.___User_input___"
+
+echo "Podaj liczbe lat"
+read age
+echo "Masz $age lat"
+
+echo "Loops, Array, Condition, Function, String Lenght, Extract String,"
+echo "Find and replac string, Concacente String,IF func, Recursion"
+echo "Create dir, Read files, Print files with line count"
+echo "Delete files, Test if exist, update package, Show server informaction"
+echo "______________________________________________________________________"
+
+
+#https://www.hostinger.com/tutorials/bash-script-example
